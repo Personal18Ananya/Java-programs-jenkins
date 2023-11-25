f26eb271fa9762f50d8b47a341cf9af1d11a66b8
Ananya Srivastava <annanya.sri18@gmail.com>
2023-11-25 13:20:08 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class Main{
+    public static void main(String args[])
+    {
+        System.out.println("hello World");
+        System.out.println("This is teh output from java program");
+        System.out.println(new Date());
+        System.out.println("After creating it as a git repository");
+ }
+}
