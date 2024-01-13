f3223d8e933516fda60fe6cb05c3733d6039a5f3
Ananya Srivastava <annanya.sri18@gmail.com>
2024-01-13 21:02:26 +0530

    Updated some spelling

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String args[])
     {
         System.out.println("hello World");
-        System.out.println("This is teh output from java program");
+        System.out.println("This is the output from java program");
         System.out.println(new Date());
         System.out.println("After creating it as a git repository");
         System.out.println("new file added");
